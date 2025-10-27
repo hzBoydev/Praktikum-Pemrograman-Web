7003869a9c1b5a1c0b38edc4b5841b54d6c7e22c
hzBoy <hifzikhairi@gmail.com>
2025-10-27 20:31:41 +0800

    Add files via upload

---
+++ 02_assignment/6-increment.php
@@ -0,0 +1,10 @@
+<?php
+$x = 4;
+$x++;
+echo "Nilai x yang baru : ". $x;
+echo "<br />";
+
+$x = 4;
+$x--;
+echo "Nilai x yang baru : ". $x;
+?>
---
+++ 02_assignment/script5-4.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <title>Konversi jumlah detik ke satuan jam-menit-detik </title>
+</head>
+<body>
+    <h1>Konversi jumlah detik ke satuan jam-menit-detik</h1>
+    <?php
+    /*
+    Script ini merupakan kebalikan dari script5-3.php
+    Script ini akan mengkonversi waktu yang diketahui dalam satuan detik
+    ke dalam satuan jam-menit-detik.
+    Diketahui waktu dalam detik adalah 15789 detik, akan dikonversi ke
+    bentuk x jam, y menit dan z detik
+    */
+    ?>
+    <?php
+    $totalDetik = 15789;
+    
+    $sisa = $totalDetik % 3600;
+    $dalamJam = ($totalDetik - $sisa) / 3600;
+
+    $totalDetik = $sisa;
+    $sisa = $totalDetik % 60;
+    $dalamMenit = ($totalDetik - $sisa) / 60;
+
+    $totalDetik = $sisa;
+    $sisa = $totalDetik % 1;
+    $dalamDetik = ($totalDetik - $sisa) / 1;
+    echo "<p>Hasil konversinya adalah : ".$dalamJam." jam : ".$dalamMenit." menit : ".$dalamDetik." detik</p>";
+    ?>
+</body>
+</html>
